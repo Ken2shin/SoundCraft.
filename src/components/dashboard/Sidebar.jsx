095414813265f308bd9e675a5f24dc1468b3f498
EMANUEL CLEMENTE MARTINEZ ARGEÑAL <emanuel.martinez24@est.unanleon.edu.ni>
2026-08-05 23:19:38 -0600

    error 500 en el siderbar

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -1,6 +1,7 @@
 "use client";
+import { useState } from "react";
 import Link from "next/link";
-import { usePathname, useRouter, useState } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import {
   AudioWaveform,
   FolderKanban,
